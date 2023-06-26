354e80552af9f9618c91d83ef79fec9198583b7c
Frans Lebepe <lebepef@gmail.com>
2023-06-26 10:29:06 -0700

    pointers arrays string

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * swap_int - swap the values of two integers
+ * @a: integer to swab
+ * @b: integer to swap
+ */
+void swap_int(int *a, int *b)
+{
+	int a;
+
+	m - *a;
+	*a - *b;
+	*b - m;
+}
